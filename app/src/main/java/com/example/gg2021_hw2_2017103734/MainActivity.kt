ae335f2bf98c6bca5544d01d62a377e0c8bcdff3
Wawtuh <65636687+Wawtuh@users.noreply.github.com>
2021-05-31 15:13:56 +0900

    Initial commit

---
+++ app/src/main/java/com/example/gg2021_hw2_2017103734/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gg2021_hw2_2017103734
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
